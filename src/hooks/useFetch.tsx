e1aae68d785ebcccedbd985d75745d0981e7705b
Luca Palmarucci <lpalmarucci@gmail.com>
2023-09-28 00:44:06 +0200

    Updated useFetch hook
    Added generics, authorization flag and redirect if not logged in

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,85 +1,45 @@
-import { useEffect, useReducer, useRef } from "react";
+import { useAuthUser } from "react-auth-kit";
+import { ApiError } from "../models/ApiError";
+import { useIsAuthenticated } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../routes/common.routes";
 
-interface State<T> {
-  data?: T;
-  error?: Error;
+export default function useFetch() {
+  const navigate = useNavigate();
+  const auth = useAuthUser();
+  console.log(auth());
+
+  async function fetchData<T>(
+    url: string,
+    method: RequestInit["method"],
+    options?: RequestInit,
+    withAuthorization: boolean = true,
+  ): Promise<T> {
+    const headers: any = {
+      "Content-Type": "application/json",
+      ...options?.headers,
+    };
+    if (withAuthorization) {
+      headers["Authorization"] = "Bearer " + auth()?.access_token;
+    }
+    const response = await fetch(import.meta.env.VITE_SERVER_URL + url, {
+      ...options,
+      method,
+      headers,
+    });
+
+    if (response.status === 401) navigate(ROUTES.Login, { replace: true });
+
+    const data = (await response.json()) as T | ApiError;
+
+    if (isResponseError(data)) return Promise.reject(data as ApiError);
+
+    return Promise.resolve(data);
+  }
+
+  return fetchData;
 }
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwic3ViIjoxLCJpYXQiOjE2OTU0MDQyNDMsImV4cCI6MTY5NTQwNzg0M30.uAeX9M8WX1bIsegR5ecpzzH8W82cd24nb4grU_Mj48A";
-
-// discriminated union type
-type Action<T> =
-  | { type: "loading" }
-  | { type: "fetched"; payload: T }
-  | { type: "error"; payload: Error };
-
-export function useFetch<T = unknown>(url?: string, options?: RequestInit): State<T> {
-  // Used to prevent state update if the component is unmounted
-  const cancelRequest = useRef<boolean>(false);
-
-  const initialState: State<T> = {
-    error: undefined,
-    data: undefined,
-  };
-
-  // Keep state logic separated
-  const fetchReducer = (state: State<T>, action: Action<T>): State<T> => {
-    switch (action.type) {
-      case "loading":
-        return { ...initialState };
-      case "fetched":
-        return { ...initialState, data: action.payload };
-      case "error":
-        return { ...initialState, error: action.payload };
-      default:
-        return state;
-    }
-  };
-
-  const [state, dispatch] = useReducer(fetchReducer, initialState);
-
-  useEffect(() => {
-    // Do nothing if the url is not given
-    if (!url) return;
-
-    cancelRequest.current = false;
-
-    const fetchData = async () => {
-      dispatch({ type: "loading" });
-
-      try {
-        const response = await fetch(import.meta.env.VITE_SERVER_URL + url, {
-          ...options,
-          headers: {
-            ...options?.headers,
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-
-        const data = (await response.json()) as T;
-        if (cancelRequest.current) return;
-
-        dispatch({ type: "fetched", payload: data });
-      } catch (error) {
-        if (cancelRequest.current) return;
-
-        dispatch({ type: "error", payload: error as Error });
-      }
-    };
-
-    void fetchData();
-
-    // Use the cleanup function for avoiding a possibly...
-    // ...state update after the component was unmounted
-    return () => {
-      cancelRequest.current = true;
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [url]);
-
-  return state;
+function isResponseError(obj: any): obj is ApiError {
+  return ![200, 201].includes(obj.statusCode) && obj.message;
 }
