369d573b23f28d43baba5a891790e45452246d74
Luca Palmarucci <lpalmarucci@gmail.com>
2023-09-28 00:44:41 +0200

    Added AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,26 @@ import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import "./index.css";
 import "./i18n.tsx";
-import { Router, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.ts";
 import { ErrorBoundary } from "react-error-boundary";
+import { AuthProvider } from "react-auth-kit";
+import { ToastProvider } from "./context/Toast.context.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<h1>Something went wronngs</h1>}>
       <NextUIProvider>
-        <RouterProvider router={router} />
+        <AuthProvider
+          authType="cookie"
+          authName={"_auth"}
+          cookieDomain={window.location.hostname}
+          cookieSecure={window.location.protocol === "https:"}
+        >
+          <ToastProvider>
+            <RouterProvider router={router} />
+          </ToastProvider>
+        </AuthProvider>
       </NextUIProvider>
     </ErrorBoundary>
   </React.StrictMode>,
