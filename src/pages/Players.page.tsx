50026469e741ba7e710438fea839c3fe021677bf
Luca Palmarucci <lpalmarucci@gmail.com>
2023-09-28 00:45:14 +0200

    Show players list

--- src/pages/Players.page.tsx
+++ src/pages/Players.page.tsx
@@ -1,27 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PlayerCard from "../components/PlayerCard.component";
 import { Player } from "../models/Player";
-import { useFetch } from "../hooks/useFetch";
-import { ApiEndpoint } from "../models/constats";
+import useFetch from "../hooks/useFetch";
+import { ApiEndpoint } from "../models/constants";
 import { motion } from "framer-motion";
-import { fadeIn, fadeOut, fadeVariant } from "../costants/animation";
+import { fadeVariant } from "../costants/animation";
+import Title from "../components/Title.component";
+import { useTranslation } from "react-i18next";
 
 const PlayersPage = () => {
-  const { data, error } = useFetch<Player[]>(ApiEndpoint.getPlayers);
+  const { t } = useTranslation();
+  const [players, setPlayers] = useState<Player[]>([]);
+  const fetchData = useFetch();
+
+  useEffect(() => {
+    fetchData<Player[]>(ApiEndpoint.getPlayers, "get")
+      .then((data) => setPlayers(data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <div className="flex flex-col gap-[4rem] text-center w-full px-6">
-      <h1 className="text-white text-5xl font-bold">Lista Giocatori</h1>
-      {data && (
+    <div className="flex flex-col gap-[4rem] text-center w-full pt-16">
+      <Title>{t("players.list")}</Title>
+      {players && (
         <motion.div
           className="w-full max-w-xl md:max-w-full flex flex-wrap gap-6 items-center justify-center"
           variants={fadeVariant}
           initial="hidden"
           animate="show"
         >
-          {data?.map((player) => <PlayerCard key={player.id} {...player} />)}
+          {players?.map((player) => <PlayerCard key={player.id} {...player} />)}
         </motion.div>
       )}
+      {/* {error && <h4 className="text-red text-2xl">{error.message}</h4>} */}
     </div>
   );
 };
