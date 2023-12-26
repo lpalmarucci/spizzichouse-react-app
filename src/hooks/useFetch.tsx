84dfdbe164f93147e5a3251dfcd25137cefc8e47
Luca Palmarucci <lpalmarucci@gmail.com>
2023-12-26 19:47:42 +0100

    Refactor useFetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,27 +1,26 @@
-import { useAuthUser } from "react-auth-kit";
-import { ApiError } from "../models/ApiError";
-import { useIsAuthenticated } from "react-auth-kit";
-import { useNavigate } from "react-router-dom";
-import { ROUTES } from "../routes/common.routes";
-import { useToast } from "../context/Toast.context";
+import { useAuthUser } from 'react-auth-kit';
+import { ApiError } from '../models/ApiError';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../routes/common.routes';
+import { useToast } from '../context/Toast.context';
 
 export default function useFetch() {
   const navigate = useNavigate();
   const auth = useAuthUser();
-  const { add } = useToast();
+  const { showAlertMessage } = useToast();
 
   async function fetchData<T>(
     url: string,
-    method: RequestInit["method"],
+    method: RequestInit['method'],
     options?: RequestInit,
     withAuthorization: boolean = true,
   ): Promise<T> {
-    const headers: any = {
-      "Content-Type": "application/json",
+    const headers: HeadersInit = new Headers({
+      'Content-Type': 'application/json',
       ...options?.headers,
-    };
+    });
     if (withAuthorization) {
-      headers["Authorization"] = "Bearer " + auth()?.access_token;
+      headers.set('Authorization', 'Bearer ' + auth()?.access_token);
     }
     const response = await fetch(import.meta.env.VITE_SERVER_URL + url, {
       ...options,
@@ -34,7 +33,7 @@ export default function useFetch() {
     const data = (await response.json()) as T | ApiError;
 
     if (isResponseError(data)) {
-      add({ message: data.message, type: "error" });
+      showAlertMessage({ message: data.message, type: 'error' });
       return Promise.reject(data as ApiError);
     }
 
