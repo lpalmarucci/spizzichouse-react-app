8891cc277343fbbb74e223edbd092600b927d5d3
Luca Palmarucci <lpalmarucci@gmail.com>
2023-12-27 17:40:25 +0100

    Added users endpoints

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,4 +1,10 @@
 export const ApiEndpoint = {
-  getUsers: '/users',
+  //Authentication
   login: '/auth/login',
+  //User
+  getUsers: '/users',
+  getSingleUser: '/users/:id',
+  createUser: '/users',
+  updateUser: '/users/:id',
+  deleteUSer: '/users/:id',
 };
