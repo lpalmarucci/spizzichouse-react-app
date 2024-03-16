b12b72b740117322605772b5d968a6f4bf2f01ff
Luca Palmarucci <lpalmarucci@gmail.com>
2024-03-16 22:57:08 +0100

    fix: removed fragment on Match List component

--- src/components/Match/MatchList.component.tsx
+++ src/components/Match/MatchList.component.tsx
@@ -11,25 +11,23 @@ function MatchList({
 }) {
   const { t } = useTranslation();
   return (
-    <>
-      <div className="w-full  max-w-6xl">
-        <div className="grid grid-cols-none sm:grid-cols-2 lg:grid-cols-4 gap-2 gap-y-10 overflow-auto p-8">
-          {matches.length > 0 ? (
-            matches.map((match) => (
-              <MatchCard
-                key={match.id}
-                match={match}
-                getAllMatches={getAllMatches}
-              />
-            ))
-          ) : (
-            <h1 className="w-full text-center text-xl col-span-full">
-              {t('matches.not_available')}
-            </h1>
-          )}
-        </div>
+    <div className="w-full  max-w-6xl">
+      <div className="grid grid-cols-none sm:grid-cols-2 lg:grid-cols-4 gap-2 gap-y-10 overflow-auto p-8">
+        {matches.length > 0 ? (
+          matches.map((match) => (
+            <MatchCard
+              key={match.id}
+              match={match}
+              getAllMatches={getAllMatches}
+            />
+          ))
+        ) : (
+          <h1 className="w-full text-center text-xl col-span-full">
+            {t('matches.not_available')}
+          </h1>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
