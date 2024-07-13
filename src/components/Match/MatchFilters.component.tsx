9d93d3d2dba06cf150596616cadc268e72fcde54
Luca Palmarucci <lpalmarucci@gmail.com>
2024-07-13 09:55:39 +0200

    added translations

--- src/components/Match/MatchFilters.component.tsx
+++ src/components/Match/MatchFilters.component.tsx
@@ -5,6 +5,7 @@ import { Player } from '../../models/Player.ts';
 import useFetch from '../../hooks/useFetch.tsx';
 import ApiEndpoints from '../../costants/ApiEndpoints.ts';
 import { Location } from '../../models/Location.ts';
+import { useTranslation } from 'react-i18next';
 
 export interface IMatchFilters {
   user?: Set<number>;
@@ -26,18 +27,17 @@ function MatchFilters(props: IMatchFiltersProps) {
   const [locations, setLocations] = useState<Location[]>([]);
 
   const fetchData = useFetch();
+  const { t } = useTranslation();
 
   useEffect(() => {
     async function fetchUsers() {
       const users = await fetchData<Player[]>(ApiEndpoints.getUsers, 'GET');
       setUsers(users);
-      return Promise.resolve();
     }
 
     async function fetchLocations() {
       const locations = await fetchData<Location[]>(ApiEndpoints.getLocations, 'GET');
       setLocations(locations);
-      return Promise.resolve();
     }
 
     Promise.all([fetchUsers(), fetchLocations()]);
@@ -63,8 +63,8 @@ function MatchFilters(props: IMatchFiltersProps) {
           <div className="flex flex-col w-full gap-6 min-h-unit-8 px-0 md:pb-4 pb-6">
             <div className="flex flex-wrap gap-3 md:gap-4 md:flex-row items-center">
               <Select
-                label="Utenti"
-                placeholder="Seleziona un utente"
+                label={t('labels.players')}
+                placeholder={t('placeholders.selectUser')}
                 className="flex-1"
                 variant="bordered"
                 key="utenti"
@@ -80,13 +80,13 @@ function MatchFilters(props: IMatchFiltersProps) {
                   ))
                 ) : (
                   <SelectItem isDisabled key="">
-                    Nessun utente disponibile
+                    {t('players.not_available')}
                   </SelectItem>
                 )}
               </Select>
               <Select
-                label="Location"
-                placeholder="Seleziona una location"
+                label={t('labels.location')}
+                placeholder={t('placeholders.selectLocation')}
                 className="flex-1"
                 variant="bordered"
                 key="locations"
@@ -98,13 +98,13 @@ function MatchFilters(props: IMatchFiltersProps) {
                   locations.map((location) => <SelectItem key={location.id}>{location.name}</SelectItem>)
                 ) : (
                   <SelectItem isDisabled key="">
-                    Nessuna location disponibile
+                    {t('locations.not_available')}
                   </SelectItem>
                 )}
               </Select>
               <Select
-                label="Stato"
-                placeholder="Stato della partita"
+                label={t('labels.status')}
+                placeholder={t('placeholders.selectStatus')}
                 className="flex-1"
                 variant="bordered"
                 key="status"
@@ -112,16 +112,16 @@ function MatchFilters(props: IMatchFiltersProps) {
                 selectedKeys={filters.inProgress}
                 onSelectionChange={(status) => setFilters((prev) => ({ ...prev, inProgress: status as Set<string> }))}
               >
-                <SelectItem key={'true'}>In corso</SelectItem>
-                <SelectItem key={'false'}>Terminate</SelectItem>
+                <SelectItem key={'true'}>{t('matches.labels.inProgress')}</SelectItem>
+                <SelectItem key={'false'}>{t('matches.labels.ended')}</SelectItem>
               </Select>
             </div>
             <div className="flex gap-2 items-center">
               <Button variant="ghost" color="primary" size="md" className="w-fit" onClick={handleSubmitFilters}>
-                Ricerca
+                {t('buttons.search')}
               </Button>
               <Button variant="light" size="md" className="w-fit" onClick={handleResetFilters}>
-                Reset
+                {t('buttons.reset')}
               </Button>
             </div>
           </div>
